8ace7538778eebed273fed25b4c8dd768ad5e559
tacocat <abhyudaypandeyofficial@gmail.com>
2025-08-23 19:57:39 +0530

    updated the code

--- fulladder_using_halfadder.v
+++ fulladder_using_halfadder.v
@@ -2,11 +2,12 @@ module fulladder(a,b,cin,cout,sum);
   input a,b,cin;
   output sum,cout;
   wire sum1;
-  wire cout1;
+  wire cout1,cout2;
 
   halfadder ins1(.a(a),.b(b),.sum(sum1), .cout(cout1)); //used the halfadder modules to instantiate a fulladder
-  halfadder ins2(.a(sum1),.b(cin),.sum(sum), .cout(cout)); //the sum output of ins1 will be the input a for ins2
-  
+  halfadder ins2(.a(sum1),.b(cin),.sum(sum), .cout(cout2)); //the sum output of ins1 will be the input a for ins2
+
+  assign cout = cout1 | cout2;
 endmodule
 
 module halfadder(a,b,sum,cout);
